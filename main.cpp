fd7f81e9831b116e65446708042947914aa51d66
Oleksandr Stelmakh <oleksandr.stelmakh@obreey-products.com>
2023-01-23 14:06:59 +0200

    lesson 3 (insertsort)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,27 @@
 #include <iostream>
 using namespace std;
 
+/*
+    arr[] = {4 7 2 3 8 1}
+
+       4   7    2 3 8 1
+
+       index = 1
+
+*/
+
+void sortInsert(std::vector<int>& arr)
+{
+    int keyValue, j;
+    for (int i = 0; i < arr.size(); i++) {
+        keyValue = arr[i];
+        for (j = i - 1; j >= 0 && arr[j] > keyValue; j--) {
+            arr[j + 1] = arr[j];
+        }
+        arr[j + 1] = keyValue;
+    }
+}
+
 void sort(std::vector<int>& arr)
 {
     for (int i = 0; i < arr.size(); i++) {
@@ -19,12 +40,15 @@ int main()
 {
     srand(time(0));
     SortHelper helper;
-    vector<int> arr;
+    vector<int> arr, arr2;
 
-    helper.generate(arr, 10000, 100);
-    helper.displayTime(arr, sort);
+    helper.generate(arr, 100000, 100);
+    arr2 = arr;
+    helper.displayTime(arr, sortInsert);
+    helper.displayTime(arr2, sort);
     //    helper.compare(arr, sort, sort2);
     //    helper.display(arr);
+    //    helper.display(arr2);
     //    helper.displayTime(sort(arr));
 
     return 0;
